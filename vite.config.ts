4bda77e9629696688b3b3a933c1799e82e3934c1
jlpereira <pepelui@outlook.com>
2024-12-04 11:53:36 -0300

    Fix action

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.ts'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const base = process.env.VITE_BASE_URL || '/'
+  const config = {
+    base,
+    plugins: [vue()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/setupTests.ts'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
+
+  return config
 })
